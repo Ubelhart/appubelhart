5511fb6c01e3c71b7e9d5e4ce9a1d87b0d2ea025
Juan Pablo Ubelhart <juanpabloubelhart@hotmail.com>
2021-11-22 20:38:27 -0300

    v0.2: Creando componente Nav

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/#">
+          E-Commerce
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/#">
+                Inicio
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/#">
+                Características
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/#">
+                Precios
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
